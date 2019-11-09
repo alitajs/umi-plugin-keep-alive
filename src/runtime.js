1fae43de4d9f4b66049010f23b12f603d1b67270
CJY <375564567@qq.com>
2019-11-09 23:20:44 +0800

    基于 react-activation 重新实现 umi-plugin-keep-alive

--- src/runtime.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export function rootContainer(container) {
-  const KeepAliveProvider = require('@tmp/KeepAliveProvider').default;
-  return React.createElement(KeepAliveProvider, null, container);
-}
