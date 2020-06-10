41dc2190ceb50b09ea5c8563333a079aced268a1
CJY <375564567@qq.com>
2020-06-10 16:23:20 +0800

    支持 umi3.x, 暂时关闭测试功能

--- src/wrapChildrenWithAliveScope.js
+++
@@ -1,7 +0,0 @@
-import { AliveScope } from 'react-activation'
-
-export default Component => ({ children, ...props }) => (
-  <Component {...props}>
-    <AliveScope>{children}</AliveScope>
-  </Component>
-)
