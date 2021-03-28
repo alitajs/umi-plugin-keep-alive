b4386ce6c2f890ee6473b7509ec06f38211c8f0a
CJY <375564567@qq.com>
2021-03-28 20:03:12 +0800

    0.9.0 不再需要想方设法绞尽脑汁放置 AliveScope 外层

--- src/index.js
+++ src/index.js
@@ -1,39 +1,39 @@
-import { utils } from 'umi'
-import { join } from 'path'
-import { readFileSync } from 'fs'
-import { getFile } from '@umijs/utils'
+// import { utils } from 'umi'
+// import { join } from 'path'
+// import { readFileSync } from 'fs'
+// import { getFile } from '@umijs/utils'
 
 export default (api) => {
-  api.addRuntimePlugin({
-    fn: () => '@@/plugin-keep-alive/runtime',
-    stage: -1 * Number.MAX_SAFE_INTEGER,
-  })
+//   api.addRuntimePlugin({
+//     fn: () => '@@/plugin-keep-alive/runtime',
+//     stage: -1 * Number.MAX_SAFE_INTEGER,
+//   })
 
-  // 因 keep-alive 的 runtime 部分选择不渲染其 children，可能会丢失默认的用户 rootContainer
-  // 此处修复自定义 rootContainer
-  // https://github.com/umijs/umi/blob/master/packages/preset-built-in/src/plugins/generateFiles/core/plugin.ts#L23-L27
-  const customizedAppPath = getFile({
-    base: api.paths.absSrcPath,
-    fileNameWithoutExt: 'app',
-    type: 'javascript',
-  })?.path
+//   // 因 keep-alive 的 runtime 部分选择不渲染其 children，可能会丢失默认的用户 rootContainer
+//   // 此处修复自定义 rootContainer
+//   // https://github.com/umijs/umi/blob/master/packages/preset-built-in/src/plugins/generateFiles/core/plugin.ts#L23-L27
+//   const customizedAppPath = getFile({
+//     base: api.paths.absSrcPath,
+//     fileNameWithoutExt: 'app',
+//     type: 'javascript',
+//   })?.path
 
-  if (customizedAppPath) {
-    api.addRuntimePlugin({
-      fn: () => '@@/plugin-keep-alive/fixCustomizedRuntime',
-      stage: -1 * Number.MAX_SAFE_INTEGER + 1,
-    })
+//   if (customizedAppPath) {
+//     api.addRuntimePlugin({
+//       fn: () => '@@/plugin-keep-alive/fixCustomizedRuntime',
+//       stage: -1 * Number.MAX_SAFE_INTEGER + 1,
+//     })
 
-    api.onGenerateFiles(async () => {
-      api.writeTmpFile({
-        path: 'plugin-keep-alive/fixCustomizedRuntime.tsx',
-        content: utils.Mustache.render(
-          readFileSync(join(__dirname, 'fixCustomizedRuntime.tsx.tpl'), 'utf-8'),
-          {},
-        ),
-      })
-    })
-  }
+//     api.onGenerateFiles(async () => {
+//       api.writeTmpFile({
+//         path: 'plugin-keep-alive/fixCustomizedRuntime.tsx',
+//         content: utils.Mustache.render(
+//           readFileSync(join(__dirname, 'fixCustomizedRuntime.tsx.tpl'), 'utf-8'),
+//           {},
+//         ),
+//       })
+//     })
+//   }
 
   // Babel Plugin for react-activation
   api.modifyBabelOpts((babelOpts) => {
@@ -50,10 +50,10 @@ export default (api) => {
     },
   ])
 
-  api.onGenerateFiles(async () => {
-    api.writeTmpFile({
-      path: 'plugin-keep-alive/runtime.tsx',
-      content: utils.Mustache.render(readFileSync(join(__dirname, 'runtime.tsx.tpl'), 'utf-8'), {}),
-    })
-  })
+//   api.onGenerateFiles(async () => {
+//     api.writeTmpFile({
+//       path: 'plugin-keep-alive/runtime.tsx',
+//       content: utils.Mustache.render(readFileSync(join(__dirname, 'runtime.tsx.tpl'), 'utf-8'), {}),
+//     })
+//   })
 }
