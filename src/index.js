c52820b49cdc5b7070ec2f388f11cb02c2ca799d
CJY <375564567@qq.com>
2021-03-29 20:20:52 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-// import { utils } from 'umi'
-// import { join } from 'path'
-// import { readFileSync } from 'fs'
+import { utils } from 'umi'
+import { join } from 'path'
+import { readFileSync } from 'fs'
 // import { getFile } from '@umijs/utils'
 
 export default (api) => {
-//   api.addRuntimePlugin({
-//     fn: () => '@@/plugin-keep-alive/runtime',
-//     stage: -1 * Number.MAX_SAFE_INTEGER,
-//   })
+  api.addRuntimePlugin({
+    fn: () => '@@/plugin-keep-alive/runtime',
+    stage: -1 * Number.MAX_SAFE_INTEGER,
+  })
 
 //   // 因 keep-alive 的 runtime 部分选择不渲染其 children，可能会丢失默认的用户 rootContainer
 //   // 此处修复自定义 rootContainer
@@ -50,10 +50,10 @@ export default (api) => {
     },
   ])
 
-//   api.onGenerateFiles(async () => {
-//     api.writeTmpFile({
-//       path: 'plugin-keep-alive/runtime.tsx',
-//       content: utils.Mustache.render(readFileSync(join(__dirname, 'runtime.tsx.tpl'), 'utf-8'), {}),
-//     })
-//   })
+  api.onGenerateFiles(async () => {
+    api.writeTmpFile({
+      path: 'plugin-keep-alive/runtime.tsx',
+      content: utils.Mustache.render(readFileSync(join(__dirname, 'runtime.tsx.tpl'), 'utf-8'), {}),
+    })
+  })
 }
