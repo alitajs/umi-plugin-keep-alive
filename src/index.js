726295a8b3ae2d6ab782d2441cca2fff3733d6d7
Baic <303394539@qq.com>
2022-07-06 13:09:03 +0800

    兼容 umi@4 插件加载方式与导出方式

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
-import { utils } from 'umi'
-import { join } from 'path'
-import { readFileSync } from 'fs'
-// import { getFile } from '@umijs/utils'
+import {
+  join
+} from 'path'
+import {
+  readFileSync,
+} from 'fs'
+import {
+  Mustache
+} from '@umijs/utils';
+
+const Encoding = 'utf-8';
 
 export default (api) => {
-  api.addRuntimePlugin({
-    fn: () => '@@/plugin-keep-alive/runtime',
-    stage: -1 * Number.MAX_SAFE_INTEGER,
-  })
+  // 通过新增方法判断umi4
+  const isUmi4 = typeof api.modifyAppData === 'function';
+
+  if (isUmi4) {
+    // umi@4 自动生成插件目录
+    api.addRuntimePlugin({
+      fn: () => '@@/plugin-keepAlive/runtime',
+      stage: -1 * Number.MAX_SAFE_INTEGER,
+    })
+  } else {
+    api.addRuntimePlugin({
+      fn: () => '@@/plugin-keep-alive/runtime',
+      stage: -1 * Number.MAX_SAFE_INTEGER,
+    })
+  }
 
   //   // 因 keep-alive 的 runtime 部分选择不渲染其 children，可能会丢失默认的用户 rootContainer
   //   // 此处修复自定义 rootContainer
@@ -27,8 +45,8 @@ export default (api) => {
   //     api.onGenerateFiles(async () => {
   //       api.writeTmpFile({
   //         path: 'plugin-keep-alive/fixCustomizedRuntime.tsx',
-  //         content: utils.Mustache.render(
-  //           readFileSync(join(__dirname, 'fixCustomizedRuntime.tsx.tpl'), 'utf-8'),
+  //         content: Mustache.render(
+  //           readFileSync(join(__dirname, 'fixCustomizedRuntime.tsx.tpl'), Encoding),
   //           {},
   //         ),
   //       })
@@ -37,21 +55,33 @@ export default (api) => {
 
   // Babel Plugin for react-activation
   const babelOpt = [require.resolve('react-activation/babel')]
-  if (api.modifyBabelOpts && typeof api.modifyBabelOpts === 'function') {
+  if (typeof api.modifyBabelOpts === 'function') {
     api.modifyBabelOpts((babelOpts) => {
       babelOpts.plugins.push(babelOpt)
       return babelOpts
     })
   }
 
-  if (api.addExtraBabelPlugins && typeof api.addExtraBabelPlugins === 'function') {
+  if (typeof api.addExtraBabelPlugins === 'function') {
     api.addExtraBabelPlugins(() => [babelOpt])
   }
   // 生成：export * from 'react-activation'
   // TODO: 指明支持的 api，使用上述方式存在 bug https://github.com/guybedford/es-module-lexer/issues/76
   // 业务中可 import { KeepAlive } from 'umi'
-  api.addUmiExports(() => [
-    {
+  if (isUmi4) {
+    // umi@4 需要通过插件文件进行自动导出
+    api.onGenerateFiles(async () => {
+      api.writeTmpFile({
+        path: 'index.ts',
+        content: Mustache.render(readFileSync(join(__dirname, 'index.ts.tpl'), Encoding), {}),
+      })
+      api.writeTmpFile({
+        path: 'runtime.tsx',
+        content: Mustache.render(readFileSync(join(__dirname, 'runtime.tsx.tpl'), Encoding), {}),
+      })
+    })
+  } else {
+    api.addUmiExports(() => [{
       specifiers: [
         'KeepAlive',
         'useActivate',
@@ -61,13 +91,13 @@ export default (api) => {
         'useAliveController',
       ],
       source: 'react-activation',
-    },
-  ])
+    }, ])
 
-  api.onGenerateFiles(async () => {
-    api.writeTmpFile({
-      path: 'plugin-keep-alive/runtime.tsx',
-      content: utils.Mustache.render(readFileSync(join(__dirname, 'runtime.tsx.tpl'), 'utf-8'), {}),
+    api.onGenerateFiles(async () => {
+      api.writeTmpFile({
+        path: 'plugin-keep-alive/runtime.tsx',
+        content: Mustache.render(readFileSync(join(__dirname, 'runtime.tsx.tpl'), Encoding), {}),
+      })
     })
-  })
+  }
 }
---
+++ src/index.ts.tpl
@@ -0,0 +1,1 @@
+export { KeepAlive, useActivate, useUnactivate, withActivation, withAliveScope, useAliveController } from 'react-activation';
